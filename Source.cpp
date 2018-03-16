e76f7b48eeb5038bbcb8f3a8435ce922c25c28b9
thoailinh <34635185+thoailinh@users.noreply.github.com>
2018-03-16 21:45:11 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -5,19 +5,18 @@ using namespace std;
 
 int main()
 {
-	int n;
+	int n; int x;//khai báo số công việc
 	cin >> n;
-	vector<pair<int, int>> dscv;
+	vector<pair<int, int>> dscv;//danh sách công việc kèm theo số thứ tự
 	for (int i = 0; i < n; i++)
 	{
-		int x;
 		cin >> x;
 		dscv.push_back({ x, i });
 	}
-	stable_sort(dscv.begin(), dscv.end());
+	stable_sort(dscv.begin(), dscv.end());// sắp xếp danh sách công việc theo thời gian tăng dần 
 	for (auto i : dscv)
 	{
-		cout << i.second;
+		cout << i.second;// xuất số thứ tự của từng máy
 	}
 	system("pause");
 	return 0;
